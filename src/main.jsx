4cf1735e24ebf895e1940f9adf673ec5f83f6ab7
Ronnie Rios <ronniemarkrios@gmail.com>
2024-03-05 20:19:00 -0600

    note 1 about the index and main files

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+//points to our index.html file, and gets appended
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    
     <App />
   </React.StrictMode>,
 )
+
+//APP
+//the first real component, this gets RENDERED on the DOM
