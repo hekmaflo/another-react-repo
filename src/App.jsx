48f6207322d0ef530410641b3b98590e66e35c39
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2024-03-21 13:58:53 -0500

    rendering some data

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import { useState } from "react"; //hook => helper funcs to do react-y things
 import TodoContainer from "./components/TodoContainer";
 import TodoCard from "./components/TodoCard";
+import TodoContainerHw from "./components/TodoContainerHw";
 const fakeData = [
-	{ id: 1, task: "task-1" },
-	{ id: 2, task: "task-2" },
-	{ id: 3, task: "task-3" },
-	{ id: 4, task: "task-4" },
+  { id: 1, task: "task-1" },
+  { id: 2, task: "task-2" },
+  { id: 3, task: "task-3" },
+  { id: 4, task: "task-4" },
 ];
 //gateway to house other components or pages
 
@@ -17,43 +18,43 @@ const fakeData = [
 //can only return ONE html element
 
 function App() {
-	//state = data
-	const [count, setCount] = useState(0) //returns an array thats two items , array destructring
-	//initial state is set when you pass in an argument to the useState()
-	//best practice for naming state, is after the resource. i.e [dogs, setDogs]
-	//useState returns an arr of two items, the current state will always be first, the setter function
-	//current state = data in that moment
-	//setter function = function that will UPDATE with something new, causing a rerender, anytime you call the setState function it will always re-render
+  //state = data
+  const [count, setCount] = useState(0); //returns an array thats two items , array destructring
+  //initial state is set when you pass in an argument to the useState()
+  //best practice for naming state, is after the resource. i.e [dogs, setDogs]
+  //useState returns an arr of two items, the current state will always be first, the setter function
+  //current state = data in that moment
+  //setter function = function that will UPDATE with something new, causing a rerender, anytime you call the setState function it will always re-render
 
-	function increaseCount() {
-		console.log('button working');
-		//to update our count, we need to use the setter function
-		setCount(count + 1) //hey this is a setState func, im going to compare the last => to the current and rerender if it's different
-	
-	}
+  function increaseCount() {
+    console.log("button working");
+    //to update our count, we need to use the setter function
+    setCount(count + 1); //hey this is a setState func, im going to compare the last => to the current and rerender if it's different
+  }
 
-
-	return (
-		<>
-			<main className="max-w-2xl mx-auto my-2">
-				<div className="my-2">
-					<h1 className="text-4xl">Counter</h1>
-					{count}
-					<button onClick={increaseCount}>Increment</button>
-				</div>
-				<h2 className="text-xl font-bold">ToDo List</h2>
-				<TodoContainer />
-				<section className="text-center mt-4">
-
-				<h3 className="text-lg font-semibold">ToD list with props example- this don&apos;t scale</h3>
-				<TodoCard todo={fakeData[0]} />
-				<TodoCard todo={fakeData[1]} />
-				<TodoCard todo={fakeData[2]} />
-				<TodoCard todo={fakeData[3]} />
-				</section>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <main className="max-w-2xl mx-auto my-2">
+        <div className="my-2">
+          <h1 className="text-4xl">Counter</h1>
+          {count}
+          <button onClick={increaseCount}>Increment</button>
+        </div>
+        <h2 className="text-xl font-bold">ToDo List</h2>
+        <TodoContainer />
+        <section className="text-center mt-4">
+          <h3 className="text-lg font-semibold">
+            ToD list with props example- this don&apos;t scale
+          </h3>
+          <TodoCard todo={fakeData[0]} />
+          <TodoCard todo={fakeData[1]} />
+          <TodoCard todo={fakeData[2]} />
+          <TodoCard todo={fakeData[3]} />
+        </section>
+        <TodoContainerHw />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/components/TodoContainerHw.jsx
+++ src/components/TodoContainerHw.jsx
@@ -1,5 +1,27 @@
 import React, { useState } from "react";
 
-function TodoContainerHw() {}
+const myData = [
+  { id: 1, task: "input more taks" },
+  { id: 2, task: "input even more taks" },
+];
+
+function TodoContainerHw() {
+  const [data, setData] = useState(myData);
+
+  return (
+    <div>
+      <h1>ToDo list</h1>
+      {myData.map((data) => {
+        return <div key={data.id}>{data.task}</div>;
+      })}
+
+      <form action="">
+        <input type="text" name="task" placeholder="text here" />
+        <input type="text" name="task" placeholder="text here" />
+        <button>Add task</button>
+      </form>
+    </div>
+  );
+}
 
 export default TodoContainerHw;
