e9fd6060efc31a006a75ca3007000b93fa5e60bd
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2024-03-21 21:04:44 -0500

    challenge

--- src/components/TodoContainerHw.jsx
+++ src/components/TodoContainerHw.jsx
@@ -1,23 +1,56 @@
 import React, { useState } from "react";
 
 const myData = [
-  { id: 1, task: "input more taks" },
-  { id: 2, task: "input even more taks" },
+  { id: 1, task: "input more task", date: "03/10/23" },
+  { id: 2, task: "input even more task", date: "03/11/23" },
 ];
 
 function TodoContainerHw() {
   const [data, setData] = useState(myData);
+  const [form, setForm] = useState({ id: "", task: "", date: "" });
+
+  function changeHandler(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  }
+  console.log(form);
+
+  function submitHandler(e) {
+    e.preventDefault();
+    setData([...data, form]);
+    setForm({ id: "", task: "", date: "" });
+  }
 
   return (
     <div>
       <h1>ToDo list</h1>
-      {myData.map((data) => {
-        return <div key={data.id}>{data.task}</div>;
+      {data.map((data) => {
+        return (
+          <div key={data.id}>
+            <p>{data.task}</p>
+            <p>{data.date}</p>
+          </div>
+        );
       })}
 
-      <form action="">
-        <input type="text" name="task" placeholder="text here" />
-        <input type="text" name="task" placeholder="text here" />
+      <form action="" onSubmit={submitHandler}>
+        <label htmlFor="">What to do?:</label>
+        <input
+          onChange={(event) => changeHandler(event)}
+          type="text"
+          name="task"
+          value={form.task}
+          placeholder="text here"
+        />
+        <label htmlFor="">What to date?:</label>
+        <input
+          onChange={(event) => changeHandler(event)}
+          type="text"
+          name="date"
+          value={form.date}
+          placeholder="text here"
+        />
         <button>Add task</button>
       </form>
     </div>
@@ -25,3 +58,4 @@ function TodoContainerHw() {
 }
 
 export default TodoContainerHw;
+//delete a task
